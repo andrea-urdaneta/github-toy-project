3ba7d61b657860b01d3e4f68103c86d2e4dee623
AndreaUrdaneta <andrea.urdaneta@helloiconic.com>
2021-10-20 13:47:27 -0400

    update folders

--- server/api/github.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-const gitHubInstance = axios.create({
-  baseURL: "https://github.com",
-});
-
-module.exports = { gitHubInstance };
--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getAuthToken } = require("../controllers/authControllers");
-
-const router = express.Router();
-
-router.post("/auth_token", getAuthToken);
-router.get("/auth_token", getAuthToken);
-
-module.exports = router;
